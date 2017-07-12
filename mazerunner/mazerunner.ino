13e512e6dc3c5423f6380416ad013da8fd0a72f2
Anish Thite <anishthite@live.com>
2017-07-12 13:10:44 -0400

    create inches conversion method and setup code

--- mazerunner/mazerunner.ino
+++ mazerunner/mazerunner.ino
@@ -1,5 +1,7 @@
-#include <TECBot_PWMServoDriver.h>
+//Mazerunner - a maze solving robot
+//Anish Thite, Jordan Schwartz, and Alex Scotte
 
+#include <TECBot_PWMServoDriver.h>
 TECBot_PWMServoDriver drive = TECBot_PWMServoDriver();
 
 //left pins
@@ -14,10 +16,35 @@ const int rightechoPin = 12; // Echo Pin of Ultrasonic Sensor
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600); // Starting Serial Terminal
+ //Initialize the PWM controller
+  drive.begin();
+  drive.setPWMFreq(60);
 
+  delay(1500);
+  //Arm the ESC's by sending a "neutral" signal for 1.5 seconds
+  drive.setDrive(0,0);
+  delay(1500);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+}
+long microsecondsToInches(long microseconds) {
+   return microseconds / 74 / 2;
+}
+long getinches(int pingPin, int echoPin){
+   long duration, inches;
+   pinMode(pingPin, OUTPUT);
+   digitalWrite(pingPin, LOW);
+   delayMicroseconds(2);
+   digitalWrite(pingPin, HIGH);
+   delayMicroseconds(10);
+   digitalWrite(pingPin, LOW);
+   pinMode(echoPin, INPUT);
+   duration = pulseIn(echoPin, HIGH);
+   //Serial.println(duration);
+   inches = microsecondsToInches(duration);
+   return inches;
+  
 }
