04adc0e54f4e36c4ad20614126099878ab48cfcd
Anish Thite <anishthite@live.com>
2017-07-12 12:33:46 -0400

    create file

---
+++ mazerunner/mazerunner.ino
@@ -0,0 +1,23 @@
+#include <TECBot_PWMServoDriver.h>
+
+TECBot_PWMServoDriver drive = TECBot_PWMServoDriver();
+
+//left pins
+const int leftpingPin = 8; // Trigger Pin of Ultrasonic Sensor  
+const int leftechoPin = 9; // Echo Pin of Ultrasonic Sensor
+//right pins
+const int centerpingPin = 8; // Trigger Pin of Ultrasonic Sensor  
+const int centerechoPin = 9; // Echo Pin of Ultrasonic Sensor
+//center pins
+const int rightpingPin = 8; // Trigger Pin of Ultrasonic Sensor  
+const int rightechoPin = 9; // Echo Pin of Ultrasonic Sensor
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
